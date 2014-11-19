77316194ac6807f26ad28ea1944ff6b14dbb8bc8
Eddie Jaoude <eddie@jaoudestudios.com>
2014-11-19 18:24:30 +0000

    Changelog page added by clicking on Build No.

---
+++ doc/PageLayout.md
@@ -0,0 +1,5 @@
+# Page / Template Layout
+
+Click on the Build / Version number to go to the Changelog page.
+
+![Page / Template Layout](/assets/readme/layout.png "Page / Template Layout")
--- src/TransformCore/Bundle/AppBundle/Controller/DefaultController.php
+++ src/TransformCore/Bundle/AppBundle/Controller/DefaultController.php
@@ -19,4 +19,17 @@ class DefaultController extends Controller
     {
         return $this->render('TransformCoreAppBundle:Default:index.html.twig', array('name' => $name));
     }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function changelogAction()
+    {
+        return $this->render(
+            'TransformCoreAppBundle:Default:changelog.html.twig',
+            array(
+                'changelog' => file('../changelog')
+            )
+        );
+    }
 }
